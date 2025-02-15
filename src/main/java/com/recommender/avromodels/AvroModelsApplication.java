95d0649b06f04e748ee5774dff96768dab07e2d1
Popa Rares Andrei <R.A.Popa@student.tudelft.nl>
2025-02-15 16:58:59 +0100

    Add initial project setup with Maven Wrapper and Avro models
    This commit initializes the project with Maven Wrapper for build management and sets up an Avro-based schema structure. It includes essential configurations such as `pom.xml`, Avro schema files (`User.avsc`, `Movie.avsc`, `Rating.avsc`), and a Spring Boot application scaffold. Additional files like `.gitignore` and test placeholders are also provided.

---
+++ src/main/java/com/recommender/avromodels/AvroModelsApplication.java
@@ -0,0 +1,13 @@
+package com.recommender.avromodels;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AvroModelsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AvroModelsApplication.class, args);
+    }
+
+}
